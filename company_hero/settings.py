0ff1ace5e1bab92c537048d98ad35353c1253703
Johann Albino <johann.albino.ti@gmail.com>
2020-08-03 13:17:45 -0300

    Ajust requirements.txt

--- company_hero/settings.py
+++ company_hero/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'rest_framework',
     'drf_yasg',
     'django_filters',
